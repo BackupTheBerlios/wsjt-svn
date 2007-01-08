5e2d9a4b56db3f5221fd7777ac6102ad7efe67ab
k1jt <k1jt@b3ca0830-8508-0410-8253-dac51d8a7955>
2007-01-08 20:02:24 +0000

    Creating a branch for the development of MAP65.

---
+++ branches/map65/runqqq.F90
@@ -0,0 +1,40 @@
+subroutine runqqq(fname,cmnd,iret)
+
+#ifdef Win32
+  use dflib
+#endif
+  integer system
+
+  character*(*) fname,cmnd
+
+#ifdef Win32
+  iret=runqq(fname,cmnd)
+#else
+  iret=system('./KVASD -q >& /dev/null')
+#endif
+
+  return
+end subroutine runqqq
+
+subroutine flushqqq(lu)
+
+#ifdef Win32
+  use dfport
+#endif
+
+  call flush(lu)
+
+  return
+end subroutine flushqqq
+
+subroutine sleepqqq(n)
+#ifdef Win32
+  use dflib
+      call sleepqq(n)
+#else
+      call usleep(n*1000)
+#endif
+
+  return
+
+end subroutine sleepqqq
