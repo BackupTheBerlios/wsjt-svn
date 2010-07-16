b3cd10eef009198a5a8688ba96fcb6848cfa737b
k1jt <k1jt@b3ca0830-8508-0410-8253-dac51d8a7955>
2010-07-16 19:15:14 +0000

    Working toward a JTMS_2a decoder.

--- wsjt7a/decodems.f90
+++ wsjt7a/decodems.f90
@@ -1,48 +1,21 @@
-subroutine decodems(dat,npts,nchar,imsg)
+subroutine decodems(dat,npts,cfile6,t2,mswidth,ndb,nrpt,Nfreeze,DFTolerance)
 
   parameter (NZ=30*11025)
   real dat(NZ)
-  character msg*28
-  complex zsum
-  integer imsg(5250)
-  real ss(0:63)
-  real*8 t
-  complex cw
-  common/mscom/cw(63,0:63)
+  integer DFTolerance
+  character*6 cfile6
 
-  nchar=5250
-  fac=1.0/300000.0
-  nerr=0
-  do k=1,nchar
-     i0=(k-1)*63
-     smax1=0.
-     do j=0,63                               !Test 64 possible characters
-        zsum=0.
-        do i=1,63                            !Sum over 63 samples
-           zsum=zsum + conjg(cw(i,j))*dat(i0+i)
-        enddo
-        sum=abs(zsum)
-        if(sum.gt.smax1) then
-           smax1=sum
-           jpk=j
-        endif
-        ss(j)=sum
+  ndf=0
+  write(*,3001) cfile6,t2,mswidth,ndb,nrpt,ndf,npts,NFreeze,DFTolerance
+3001 format(a6,f5.1,i5,i3,i3.2,i5,2x,4i6)
+! 1050    format(a6,f5.1,i5,i3,1x,i2.2,i5,1x,a3,1x,a40)
+
+  if(t2.lt.3.0) then
+     do i=1,npts
+        write(51,3002) i,dat(i)
+3002    format(i8,f10.1)
      enddo
-
-     ss(jpk)=0.
-     smax2=0.
-     do j=0,63                               !Test 64 possible characters
-        smax2=max(ss(j),smax2)
-     enddo
-     rr=smax1/smax2
-     if(jpk.ne.imsg(k)) nerr=nerr+1
-!     write(*,3001) k,imsg(k),jpk,rr
-!3001 format(3i6,f12.3)
-  enddo
-
-  ferr=float(nerr)/nchar
-  write(*,1110) nerr,ferr
-1110 format('Errors:',i5,f10.4)
+  endif
 
   return
 999 end subroutine decodems
--- wsjt7a/mtdecode.f
+++ wsjt7a/mtdecode.f
@@ -116,7 +116,12 @@ C  Assemble a signal report:
          nrpt=10*nwidth + nstrength
          t2=tstart + dt*(istart-1)
 
-         write(*,1050) cfile6,t2,mswidth,int(peak),nrpt,noffset
+         if(mode.eq.8) then
+            nfreeze=0
+            call decodems(dat(jj),jjz,cfile6,t2,mswidth,int(peak),
+     +           nrpt,nfreeze,DFTolerance)
+            go to 100
+         endif
 
          if(n441pp.eq.1 .and.mode.eq.1) call pp441(dat,jz,cfile6,    
      +         tstart,t2,width,npeak,nrpt,dftolerance,frag,0)
