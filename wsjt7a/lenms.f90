c11b6bede503c8f6a184aaaced62e6397274ae54
k1jt <k1jt@b3ca0830-8508-0410-8253-dac51d8a7955>
2010-07-30 00:26:15 +0000

    Correct upper limit of loop detecting msglen.

--- wsjt7a/lenms.f90
+++ wsjt7a/lenms.f90
@@ -11,7 +11,7 @@ subroutine lenms(r,npts,msglen)
      r=r-sum(r(1:npts))/npts
      acfmax=0.
      acf0=dot_product(r(1:npts),r(1:npts))
-     kz=min(npts/2,28*56)
+     kz=min(npts/2,29*56)
      do k=8,kz
         fac=float(npts)/(npts-k)
            acf(k)=fac*dot_product(r(1:npts),r(1+k:npts+k))/acf0
